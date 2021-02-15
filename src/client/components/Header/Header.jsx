dfb2e174caf34dc211721389a4e825f18420608f
Rabii Luena <itisrabii@gmail.com>
2021-02-15 20:11:14 +0300

    putting content to the center

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -2,14 +2,14 @@ import { Button, TimeTicker } from '@app/components';
 import { PauseIcon, BellOutlineIcon } from '@app/components/Icons';
 
 const Header = () => (
-  <div className="w-full border-b border-neutral-100 flex justify-between py-2 px-4">
+  <div className="w-full border-b border-neutral-100 flex p-2 px-4">
     <div className="items-center">
       <h2 className=" font-extrabold text-lg text-secondary-400 m-0 p-0">
         BSc and Mathematics
       </h2>
       {/* <p className="font-bold text-lg">BSc and Mathematics</p> */}
     </div>
-    <div className="flex items-center">
+    <div className="flex items-center mx-auto">
       <TimeTicker />
       &nbsp;
       <Button>
