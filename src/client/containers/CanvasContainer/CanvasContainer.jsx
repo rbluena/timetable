06a2026725fbd58fd97547173cf2c9e4f0a375cc
Rabii Luena <itisrabii@gmail.com>
2021-02-16 09:23:15 +0300

    * rendering toggle to the canvas

--- src/client/containers/CanvasContainer/CanvasContainer.jsx
+++ src/client/containers/CanvasContainer/CanvasContainer.jsx
@@ -1,9 +1,27 @@
-import { Header } from '@app/components';
+import { useState } from 'react';
+import { Header, ToggleSwitch } from '@app/components';
 
-const CanvasContainer = () => (
-  <div className="w-full">
-    <Header />
-  </div>
-);
+const CanvasContainer = () => {
+  const [toggle, setToggle] = useState('about');
+
+  return (
+    <div className="w-full h-screen">
+      <Header />
+      <div>
+        <div className="absolute bottom-4 flex ml-4">
+          <ToggleSwitch
+            options={[
+              { label: 'About', value: 'about' },
+              { label: 'Timeline', value: 'timeline' },
+              { label: 'Calendar', value: 'calendar' },
+            ]}
+            value={toggle}
+            onChange={(evt) => setToggle(evt.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CanvasContainer;
