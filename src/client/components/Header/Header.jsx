a3783e9d987ae706afce51b4d0fc6ec65be5371f
Rabii Luena <itisrabii@gmail.com>
2021-02-16 18:51:04 +0300

    * refactoring header on the canvas

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -1,26 +1,36 @@
+import PropTypes from 'prop-types';
 import { Button, TimeTicker } from '@app/components';
 import { BellOutlineIcon } from '@app/components/Icons';
 
-const Header = () => (
+const Header = ({ showTimer, heading }) => (
   <div className="w-full border-b border-neutral-100 flex justify-between p-2 px-4 relative">
-    <div className="items-center absolute">
-      <h2 className=" font-extrabold text-lg text-secondary-400 m-0 p-0">
-        BSc and Mathematics
+    <div className={`items-center ${showTimer ? 'absolute' : ''}`}>
+      <h2 className=" font-extrabold text-lg text-secondary-400 m-0 p-0 whitespace-pre-wrap">
+        {heading}
       </h2>
       {/* <p className="font-bold text-lg">BSc and Mathematics</p> */}
     </div>
-    <div className="mx-auto">
-      <TimeTicker />
-    </div>
-    <div>
-      <div>
-        <Button className="relative">
-          <span className="bg-secondary-500 h-2 w-2 rounded-full block absolute top-1 left-1" />
-          <BellOutlineIcon size="sm" variant="secondary" />
-        </Button>
+    {showTimer && (
+      <div className="mx-auto">
+        <TimeTicker />
       </div>
+    )}
+    <div>
+      <Button className="relative">
+        <span className="bg-secondary-500 h-2 w-2 rounded-full block absolute top-1 left-1" />
+        <BellOutlineIcon size="sm" variant="secondary" />
+      </Button>
     </div>
   </div>
 );
+
+Header.defaultProps = {
+  heading: '',
+};
+
+Header.propTypes = {
+  showTimer: PropTypes.string.isRequired,
+  heading: PropTypes.string,
+};
 
 export default Header;
--- src/client/containers/CanvasContainer/CanvasContainer.jsx
+++ src/client/containers/CanvasContainer/CanvasContainer.jsx
@@ -6,85 +6,84 @@ const CanvasContainer = () => {
 
   return (
     <div className="w-full h-screen">
-      <Header />
-      <div>
-        <div className="absolute bottom-4 flex flex-wrap justify-between my-4">
-          <ToggleSwitch
-            options={[
-              { label: 'About', value: 'about' },
-              { label: 'Timeline', value: 'timeline' },
-              { label: 'Calendar', value: 'calendar' },
+      <Header showTimer heading="BSc and Mathematics" />
+
+      <div className="absolute bottom-4 flex flex-wrap justify-between my-4">
+        <ToggleSwitch
+          options={[
+            { label: 'About', value: 'about' },
+            { label: 'Timeline', value: 'timeline' },
+            { label: 'Calendar', value: 'calendar' },
+          ]}
+          value={toggle}
+          onChange={(evt) => setToggle(evt.target.value)}
+        />
+
+        {/* start: Organizers section */}
+        <div className="flex flex-wrap items-center ml-0 md:ml-8">
+          <AvatarList
+            images={[
+              { initials: 'RL' },
+              {
+                src:
+                  'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                alt: 'Rabii Luena',
+              },
+              { initials: 'AL' },
             ]}
-            value={toggle}
-            onChange={(evt) => setToggle(evt.target.value)}
+            size="lg"
           />
 
-          {/* start: Organizers section */}
-          <div className="flex flex-wrap items-center ml-0 md:ml-8">
-            <AvatarList
-              images={[
-                { initials: 'RL' },
-                {
-                  src:
-                    'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-                  alt: 'Rabii Luena',
-                },
-                { initials: 'AL' },
-              ]}
-              size="lg"
+          <div className="flex ml-4">
+            <Text
+              text="Organizers"
+              variant="primary"
+              type="subheading"
+              className="font-bold"
             />
-
-            <div className="flex ml-4">
-              <Text
-                text="Organizers"
-                variant="primary"
-                type="subheading"
-                className="font-bold"
-              />
-              &nbsp; &nbsp;
-              <Text
-                type="subheading"
-                variant="neutral"
-                text="3"
-                className="font-bold"
-              />
-            </div>
-          </div>
-          {/* end: organizers section */}
-
-          {/* start: Members section */}
-          <div className="flex flex-wrap items-center ml-0 md:ml-8">
-            <AvatarList
-              images={[
-                { initials: 'RL' },
-                {
-                  src:
-                    'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-                  alt: 'Rabii Luena',
-                },
-                { initials: 'AL' },
-              ]}
-              size="lg"
+            &nbsp; &nbsp;
+            <Text
+              type="subheading"
+              variant="neutral"
+              text="3"
+              className="font-bold"
             />
-
-            <div className="flex ml-4">
-              <Text
-                text="Members"
-                variant="primary"
-                type="subheading"
-                className="font-bold"
-              />
-              &nbsp; &nbsp;
-              <Text
-                type="subheading"
-                variant="neutral"
-                text="3"
-                className="font-bold"
-              />
-            </div>
           </div>
-          {/* end: Members section */}
         </div>
+        {/* end: organizers section */}
+
+        {/* start: Members section */}
+        <div className="flex flex-wrap items-center ml-0 md:ml-8">
+          <AvatarList
+            images={[
+              { initials: 'RL' },
+              {
+                src:
+                  'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                alt: 'Rabii Luena',
+              },
+              { initials: 'AL' },
+            ]}
+            size="lg"
+          />
+
+          <div className="flex ml-4">
+            <Text
+              text="Members"
+              variant="primary"
+              type="subheading"
+              className="font-bold"
+            />
+            &nbsp; &nbsp;
+            <Text
+              type="subheading"
+              variant="neutral"
+              text="3"
+              className="font-bold"
+            />
+          </div>
+        </div>
+        {/* end: Members section */}
       </div>
     </div>
   );
