f8760583ef854addf2429b4d048dcccc6fd8b23b
Rabii Luena <itisrabii@gmail.com>
2021-02-23 12:21:30 +0300

    * improving the layout about page

--- src/client/containers/AboutContainer/AboutContainer.jsx
+++ src/client/containers/AboutContainer/AboutContainer.jsx
@@ -1,118 +1,113 @@
-import React from 'react';
-import { Button, ToggleSwitch, Text } from '@app/components';
+import React, { useState } from 'react';
+import { Typography, Radio, Button } from 'antd';
+import { Text } from '@app/components';
 import Organizers from './Organizers';
 import Members from './Members';
 
-const AboutContainer = () => (
-  <div className="bg-white mx-auto max-w-6xl mt-4 shadow-sm rounded p-4 pb-8 relative">
-    <h2 className="text-2xl py-6 text-neutral-800 font-secondary">
-      Title of the project{' '}
-    </h2>
+const { Title, Paragraph } = Typography;
 
-    <div className="flex flex-col md:flex-row md:flex-wrap">
-      <div className="flex flex-col">
-        <span className="font-bold text-lg text-success-600">Start</span>
-        <span className="font-bold text-neutral-400">Feb 21, 2022</span>
+const AboutContainer = () => {
+  const [editableTitle, setEditableTitle] = useState('Title of the project');
+  const [editableDescription, setEditableDescription] = useState(
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga laboriosam voluptas, eum aspernatur recusandae ipsam id, sint exercitationem totam quo magnam, magni delectus mollitia a dolorum. Reiciendis assumenda velit aut.'
+  );
+
+  return (
+    <div className="bg-white mx-auto max-w-6xl mt-4 shadow-sm rounded p-6 pb-8 relative">
+      <div className="max-w-2xl">
+        <Title
+          level={4}
+          editable={{ onChange: setEditableTitle }}
+          className="py-4"
+        >
+          {editableTitle}
+        </Title>
+
+        <Paragraph
+          editable={{ onChange: setEditableDescription }}
+          className=" text-neutral-800"
+        >
+          {editableDescription}
+        </Paragraph>
+
+        {/* start: toggle public vs private */}
+        <div className="py-4">
+          <Radio.Group
+            defaultValue="private"
+            value="private"
+            buttonStyle="solid"
+            // onChange={changeProjectView}
+          >
+            <Radio.Button value="private">Private</Radio.Button>
+            <Radio.Button value="public">Public</Radio.Button>
+          </Radio.Group>
+        </div>
+        {/* end: toggle public vs private */}
+
+        <div className="flex flex-col md:flex-row md:flex-wrap py-4">
+          <div className="flex flex-col">
+            <span className="font-bold text-success-600">Start</span>
+            <span className=" text-neutral-400">Feb 21, 2022</span>
+          </div>
+          <div className="flex flex-col ml-10">
+            <span className="font-bold text-success-600">End</span>
+            <span className="text-neutral-400">Feb 21, 2022</span>
+          </div>
+        </div>
+
+        {/* start: Organizers */}
+        <div className="py-6">
+          <div className="text-lg mb-2">
+            <span className="text-success-600">Organizers</span>&nbsp;
+            <span className="text-neutral-500 font-bold">0</span>
+          </div>
+          <Organizers />
+          <Button className="mt-2" size="small">
+            +&nbsp;Add
+          </Button>
+        </div>
+        {/* end: Organizers */}
+
+        {/* start: Member */}
+        <div className="py-6">
+          <div className="text-lg mb-2">
+            <span className="text-success-600">Members</span>&nbsp;
+            <span className="text-neutral-500 font-bold">0</span>
+          </div>
+          <Members />
+          <Button className="mt-2" size="small">
+            +&nbsp;Add
+          </Button>
+        </div>
+        {/* end: Member */}
+
+        {/* start: Topics */}
+        <div className="py-6">
+          <div className="text-lg mb-2">
+            <span className="text-success-600">Categories</span>&nbsp;
+          </div>
+
+          <ul className="flex">
+            <li className="px-2">
+              <Text weight="bold" variant="neutral" text="Physics" />
+            </li>
+            <li className="px-2">
+              <Text weight="bold" variant="neutral" text="Chemistry" />
+            </li>
+            <li className="px-2">
+              <Text weight="bold" variant="neutral" text="Mathematics" />
+            </li>
+            <li className="px-2">
+              <Text weight="bold" variant="neutral" text="Geography" />
+            </li>
+          </ul>
+
+          <Button size="small">+&nbsp;Add</Button>
+        </div>
+        {/* end: Member */}
       </div>
-      <div className="flex flex-col ml-10">
-        <span className="font-bold text-lg text-success-600">End</span>
-        <span className="font-bold  text-neutral-400">Feb 21, 2022</span>
-      </div>
     </div>
-
-    <div className="text-lg text-neutral-800 py-6">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga laboriosam
-      voluptas, eum aspernatur recusandae ipsam id, sint exercitationem totam
-      quo magnam, magni delectus mollitia a dolorum. Reiciendis assumenda velit
-      aut.
-      <br />
-    </div>
-
-    <div className="py-2">
-      <ToggleSwitch
-        options={[
-          { label: 'Private', value: 'private' },
-          { label: 'Public', value: 'public' },
-        ]}
-        value="private"
-      />
-    </div>
-
-    {/* start: Organizers */}
-    <div className="py-6">
-      <div className="text-xl font-bold mb-2">
-        <span className="text-success-600">Organizers</span>&nbsp;
-        <span className="text-neutral-500 font-bold">0</span>
-      </div>
-      {/*  */}
-      <Organizers />
-      {/*  */}
-      <Button
-        variant="text-button"
-        className="text-success-600 hover:underline text-sm"
-      >
-        +&nbsp;Add
-      </Button>
-    </div>
-    {/* end: Organizers */}
-
-    {/* start: Member */}
-    <div className="py-6">
-      <div className="text-xl font-bold mb-2">
-        <span className="text-success-600">Members</span>&nbsp;
-        <span className="text-neutral-500 font-bold">0</span>
-      </div>
-
-      {/* start: members */}
-      <Members />
-      {/* end: members */}
-
-      <Button
-        variant="text-button"
-        className="text-success-600 hover:underline text-sm"
-      >
-        +&nbsp;Add
-      </Button>
-    </div>
-    {/* end: Member */}
-
-    {/* start: Topics */}
-    <div className="py-6">
-      <div className="text-xl font-bold mb-2">
-        <span className="text-success-600">Categories</span>&nbsp;
-      </div>
-
-      <ul className="flex">
-        <li className="px-2">
-          <Text weight="bold" variant="neutral" text="Physics" />
-        </li>
-        <li className="px-2">
-          <Text weight="bold" variant="neutral" text="Chemistry" />
-        </li>
-        <li className="px-2">
-          <Text weight="bold" variant="neutral" text="Mathematics" />
-        </li>
-        <li className="px-2">
-          <Text weight="bold" variant="neutral" text="Geography" />
-        </li>
-      </ul>
-
-      <Button
-        variant="text-button"
-        className="text-success-600 hover:underline text-sm"
-      >
-        +&nbsp;Add
-      </Button>
-    </div>
-    {/* end: Member */}
-
-    <button
-      type="button"
-      className=" absolute top-2 right-2 rounded px-4 py-2 font-bold bg-secondary-600 hover:bg-secondary-400 text-white p-1"
-    >
-      Edit
-    </button>
-  </div>
-);
+  );
+};
 
 export default AboutContainer;
