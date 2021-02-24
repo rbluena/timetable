45b0505fedcef5b1c64bb44309fc05f6bc4587c8
Rabii Luena <itisrabii@gmail.com>
2021-02-24 11:02:44 +0300

    * remove slices

--- src/client/slices/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import authSlice from './authSlice';
-
-export default combineReducers({
-  // global: globalSlice,
-  auth: authSlice,
-  // links: linksSlice,
-  // comments: commentSlice,
-});
