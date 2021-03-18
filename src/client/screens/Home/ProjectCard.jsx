ee88f319b9f6fc2fed79f083dac885318dff6741
Rabii Luena <itisrabii@gmail.com>
2021-03-18 18:24:18 +0300

    * showing subscription label

--- src/client/screens/Home/ProjectCard.jsx
+++ src/client/screens/Home/ProjectCard.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { Tag } from 'antd';
 
 const ProjectCard = ({ project }) => (
   <div className="transition-shadow duration-150 m-2 w-64 bg-white shadow hover:shadow-xl">
     <Link href={`/projects/${project._id}`}>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a className="flex flex-col items-center justify-center p-8">
+      <a className="flex flex-col items-center justify-center p-6">
         <div className="h-20 w-20 rounded-full border-2 mb-4 border-primary-500 flex items-center justify-center text-xl text-primary-500  font-light">
           {project.code}
         </div>
@@ -15,6 +16,11 @@ const ProjectCard = ({ project }) => (
         <p className="text-neutral-500 mt-4 font-bold">Account Name</p>
       </a>
     </Link>
+    <div className="p-2">
+      <Tag color="geekblue" size="large">
+        PRO
+      </Tag>
+    </div>
   </div>
 );
 
