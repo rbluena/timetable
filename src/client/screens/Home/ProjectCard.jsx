72ba247b48e6688241afb07dc173fd7075c43c28
Rabii Luena <itisrabii@gmail.com>
2021-04-04 14:54:06 +0300

    * adding project code.

--- src/client/screens/Home/ProjectCard.jsx
+++ src/client/screens/Home/ProjectCard.jsx
@@ -1,7 +1,18 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Tag, Button } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import {} from 'lodash';
+import { Tag, Button, Tooltip } from 'antd';
+import { DeleteOutlined, TeamOutlined } from '@ant-design/icons';
+
+function getCode(string = '') {
+  return string
+    .split(' ')
+    .map((word) =>
+      word.toLowerCase() === 'of' || word.toLowerCase() === 'and'
+        ? ''
+        : word[0].toUpperCase()
+    );
+}
 
 const ProjectCard = ({ project, deleteProject }) => (
   <div className="transition-shadow duration-150 m-2 w-64 bg-white shadow hover:shadow-xl relative">
@@ -17,28 +28,30 @@ const ProjectCard = ({ project, deleteProject }) => (
 
     <Link href={`/projects/${project._id}`}>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a className="flex flex-col items-center justify-center p-6">
-        <div className="h-20 w-20 rounded-full border-2 mb-4 border-primary-500 flex items-center justify-center text-xl text-primary-500  font-light">
-          {project.code}
+      <a className="flex flex-col items-center justify-center p-4">
+        <div className="h-20 w-20 rounded-full border-2 mb-2 border-primary-500 flex items-center justify-center text-xl text-primary-500  font-light">
+          {getCode(project.title)}
         </div>
-        <h2 className="mx-auto font-bold text-md text-center text-neutral-400">
+        <h2 className="mx-auto text-md font-normal text-center text-neutral-400">
           {project.title}
         </h2>
-        <p className="text-neutral-500 mt-4 font-bold">
+        <p className="text-neutral-500 font-bold">
           {project.owner.accountName}
         </p>
       </a>
     </Link>
-    <div className="p-2">
+    <div className="p-2 flex justify-between items-center">
       {project.subscription ? (
-        <Tag color="gold" size="large">
-          {project.subscription.subscribedTo}
-        </Tag>
+        <Tag color="gold">{project.subscription.subscribedTo}</Tag>
       ) : (
-        <Tag color="default" size="large">
-          BASIC
-        </Tag>
+        <Tag color="default">BASIC</Tag>
       )}
+      <Tooltip title="Members">
+        <span className="text-neutral-400 font-bold">
+          <TeamOutlined />
+          {project.team.length}
+        </span>
+      </Tooltip>
     </div>
   </div>
 );
