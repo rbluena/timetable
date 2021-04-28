eeb80785aba159fde13e19a22f660c958c00a30a
Rabii Luena <itisrabii@gmail.com>
2021-04-28 12:05:09 +0300

    * adding project examples layout

--- src/client/screens/Index/LandingPage.jsx
+++ src/client/screens/Index/LandingPage.jsx
@@ -53,7 +53,7 @@ const Index = () => {
                   </Link>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <Link href="/examples">
+                  <Link href="/projects/examples">
                     <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
                       Examples
                     </a>
