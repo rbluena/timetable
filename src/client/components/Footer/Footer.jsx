838ad0c5992f17d9eb248b9931351f7aec02dc5c
Rabii Luena <itisrabii@gmail.com>
2021-05-02 18:25:58 +0300

    * adding social media icons

--- src/client/components/Footer/Footer.jsx
+++ src/client/components/Footer/Footer.jsx
@@ -1,8 +1,39 @@
 import { format } from 'date-fns';
 import Link from 'next/link';
+import {
+  TwitterOutlined,
+  LinkedinOutlined,
+  YoutubeOutlined,
+  InstagramOutlined,
+} from '@ant-design/icons';
 
 const Footer = () => (
   <div className="max-w-7xl mx-auto text-center py-12">
+    {/* start: Social icons */}
+    <ul className="mx-auto max-w-md text-center text-2xl font-light space-x-3">
+      <li className="inline-block">
+        <a href="https://twitter.com/asteyocompany">
+          <TwitterOutlined size="large" />
+        </a>
+      </li>
+      <li className="inline-block">
+        <a href="https://www.linkedin.com/company/asteyo/about/">
+          <LinkedinOutlined size="large" />
+        </a>
+      </li>
+      <li className="inline-block">
+        <a href="https://www.instagram.com/asteyocompany">
+          <InstagramOutlined size="large" />
+        </a>
+      </li>
+      <li className="inline-block">
+        <a href="https://www.youtube.com/asteyo">
+          <YoutubeOutlined size="large" />
+        </a>
+      </li>
+    </ul>
+    {/* end: Social icons */}
+
     <div className="">
       <ul className="space-x-2">
         <li className="inline-block text-base underline">
