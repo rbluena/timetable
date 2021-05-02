8fbdaf12954acc2bf0d797bcce90821aba2e6cbf
Rabii Luena <itisrabii@gmail.com>
2021-05-02 20:12:07 +0300

    * adding chat for customer care.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -83,6 +83,25 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          {/* Start: Tawk */}
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/608ed62462662a09efc42d80/1f4mv9094';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+            }}
+          />
+          {/* End: Tawk */}
         </body>
       </Html>
     );
